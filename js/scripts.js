d1b70e10fd13f10ab69570c355ee03851a4d5355
Sabrine <149007114+sabz22@users.noreply.github.com>
2024-04-17 13:14:03 -0500

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+const map = L.map("map").setView([41.5868, -93.625], 13);
+
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
